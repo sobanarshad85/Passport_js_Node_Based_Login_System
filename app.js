91adb5e777750901f5e46738bd60d1b50215c5d5
Soban Arshad <sobanarshad85@gmail.com>
2018-12-08 09:19:54 +0500

    package.json cahnges

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var expressValidator = require('express-validator');
 var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
-var LocalStrategy = require('passport-local');
+var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/loginapp');
